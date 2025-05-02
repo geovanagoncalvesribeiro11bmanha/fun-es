5fa7a242217e5658a48f7dab1ca74fc8a85f1ce7
Geovana Goncalves <geovanagoncalvesribeiro@eaportal.org>
2025-05-02 15:40:36 -0300

    sexto exemplo feito

--- script.js
+++ script.js
@@ -48,3 +48,11 @@ button.addEventListener("click", function (){
 })
 console.log (numero)
 */
+
+let numero = 10 
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+    numero++
+    console.log(numero)
+})
+console.log(numero)

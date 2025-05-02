c849007e98eab428707910f53cb1af4375b02ebe
Geovana Goncalves <geovanagoncalvesribeiro@eaportal.org>
2025-05-02 15:09:28 -0300

    terceiro exemplo feito

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ function mostrarAlerta () {
 mostrarAlerta()
 */
 
+/* EXEMPLO 2
 function somar (valorA, valorB){
     alert(valorA + valorB)
 }
 somar (20, 12)
+*/
+
+/* EXEMPLO 3
+function multiplicacao (numero1, numero2){
+    return numero1 * numero2
+}
+
+let multiplicacao1 = multiplicacao(6, 7)
+let multiplicacao2 = multiplicacao(9, 10)
+let multiplicacao3 = multiplicacao(10, 12)
+
+console.log(multiplicacao1)
+console.log(multiplicacao2)
+console.log(multiplicacao3)
+*/
